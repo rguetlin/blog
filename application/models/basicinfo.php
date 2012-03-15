b31a2902ecbecc0ba477a8382ed758c949248591
rguetlin <rguetlin@msn.com>
2012-03-15 08:07:30 -0700

    add application structure

---
+++ application/models/basicinfo.php
@@ -0,0 +1,18 @@
+<?php
+
+class Zend_ Application_BasicInfo
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
